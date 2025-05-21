df8b46b6db802b231006556f13d8f516e9ac47f9
victorgsn <149541202+victorgsn@users.noreply.github.com>
2025-05-20 21:54:36 -0300

    Correção

--- src/Home.js
+++ src/Home.js
@@ -13,6 +13,9 @@ const Home = () =>{
                 <li>
                     <Link to="/Atv02">Semana 2</Link>
                 </li>
+                <li>
+                    <Link to="/Atv03">Semana 3</Link>
+                </li>
             </ul>
         </nav>
     </div>
